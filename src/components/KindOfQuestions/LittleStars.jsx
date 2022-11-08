0a144a04a385dfdb1450bab44fea3dba387b03e8
LuisFOsG <luisfosgdev@gmail.com>
2022-11-07 19:52:24 -0500

    feat: tipo estrellas

--- src/components/KindOfQuestions/LittleStars.jsx
+++ src/components/KindOfQuestions/LittleStars.jsx
@@ -1,54 +1,35 @@
 import { useState } from 'react'
-import { View, TouchableOpacity, Image, FlatList, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, FlatList, StyleSheet } from 'react-native'
 
-import Text from 'components/Styled/Text'
-
-import emocionado from 'assets/smiles/emocionado.png'
-import bien from 'assets/smiles/bien.png'
+import Star from 'components/Icons/star'
+import StarSelect from 'components/Icons/starSelect'
 
 import useTheme from 'hooks/useTheme'
 import { wp, hp } from 'utils'
 
-const ImageComponent = ({ keyPress, onPress, image, label = '', value }) => {
+const ImageComponent = ({ keyPress, onPress, value }) => {
   const { styles } = useTheme(getStyles)
 
-  const InputStyles = [
-    styles.buttonSmile,
-    keyPress === value && styles.buttonPress
-  ]
-
-  const ImageStyles = [
-    styles.imageStyle,
-    keyPress === value && styles.imagePress
-  ]
-
-  const textStyles = [
-    styles.textSmile,
-    keyPress === value && styles.textPress
-  ]
+  const SelectStar = keyPress >= value ? StarSelect : Star
 
   return (
-    <TouchableOpacity onPress={() => onPress(value)} style={InputStyles}>
-      <Image
-        style={ImageStyles}
-        source={image}
-        resizeMode='contain'
-      />
-
-      <Text style={textStyles}>{label}</Text>
+    <TouchableOpacity style={styles.imgStyles} onPress={() => onPress(value)}>
+      <SelectStar width={wp('16%')} height={hp('10%')} />
     </TouchableOpacity>
   )
 }
 
 const SMILES = [
   {
-    label: 'Algunas Veces',
-    image: bien,
+    value: 1
+  },
+  {
     value: 2
   },
   {
-    label: 'Siempre',
-    image: emocionado,
+    value: 3
+  },
+  {
     value: 4
   }
 ]
@@ -65,14 +46,11 @@ const LittleStars = ({ setAnswer }) => {
   return (
     <View style={styles.container}>
       <FlatList
+        horizontal
         data={SMILES}
-        numColumns={2}
-        columnWrapperStyle={{ justifyContent: 'space-between' }}
         renderItem={({ item }) => (
           <ImageComponent
             onPress={handlePress}
-            image={item.image}
-            label={item.label}
             value={item.value}
             keyPress={keyPress}
           />
@@ -84,46 +62,13 @@ const LittleStars = ({ setAnswer }) => {
 
 const getStyles = theme => StyleSheet.create({
   container: {
-    position: 'relative',
-    height: hp('41%'),
-    width: wp('75%')
-  },
-  imagePress: {
-    position: 'absolute',
-    width: wp('29%'),
-    height: hp('15%'),
-    top: hp('-1.5%')
-  },
-  buttonPress: {
-    backgroundColor: '#EBEDF0'
-  },
-  textPress: {
-    position: 'absolute',
-    top: hp('13%'),
-    color: '#000000'
-  },
-  imageStyle: {
-    width: wp('28%'),
-    height: hp('12%')
-  },
-  buttonSmile: {
-    width: wp('35%'),
-    padding: wp('3%'),
-    marginVertical: hp('1%'),
-
     justifyContent: 'center',
     alignItems: 'center',
-
-    borderRadius: wp('2%'),
-    shadowColor: '#E5DEDE',
-    shadowOpacity: 0.2,
-    shadowRadius: 1,
-    elevation: 1.4
+    height: hp('10%'),
+    width: wp('75%')
   },
-  textSmile: {
-    marginTop: hp('0.5%'),
-    fontSize: hp('2%'),
-    fontWeight: '400'
+  imgStyles: {
+    paddingHorizontal: wp('1%')
   }
 })
 
