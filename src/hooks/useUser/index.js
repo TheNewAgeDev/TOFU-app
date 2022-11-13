d4aa824bea64fa832046854cd11d13e3ce0afeb9
LuisFOsG <luisfosgdev@gmail.com>
2022-11-13 17:46:14 -0500

    feat: estrayendo funciones a otro archivo

--- src/hooks/useUser/index.js
+++ src/hooks/useUser/index.js
@@ -1,68 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux'
 
 import {
-  login,
-  logout,
   changeSelectProgram,
   selectProgram as selectProgramReducer
 } from 'features/userSlice'
 
 import useFetch from 'hooks/useFetch'
-import { VALID_EMAIL } from 'utils'
+
+import { loginUser as login, logoutUser as logout } from './login'
 
 const useUser = () => {
   const dispatch = useDispatch()
   const user = useSelector(state => state.user)
   const { sendFetch } = useFetch()
 
-  const loginUser = async ({ email, password }, setStatus) => {
-    if (!email && !password) return setStatus('noData')
-    if (!email || !VALID_EMAIL.test(email)) return setStatus('noEmail', 'Por favor, ingrese un correo valido')
-    if (!password) return setStatus('noPassword', 'Por favor, ingrese la contraseña')
-
-    const bodyJson = {
-      email,
-      password,
-      device_name: 'app_mobile'
-    }
-
-    try {
-      setStatus('loading')
-
-      const data = await sendFetch({
-        route: '/token',
-        method: 'POST',
-        bodyJson
-      })
-
-      if (data && data.access_token && !data.errors) {
-        setStatus('success')
-        return dispatch(login(data))
-      } else if (data && !data.errors) return setStatus('error')
-
-      if (data.errors.email) return setStatus('errorEmail', 'Usuario no encontrado')
-      setStatus('errorPassword', 'Contraseña Incorrecta')
-    } catch (error) {
-      setStatus('error')
-      console.log('Ocurrio un Error => ', error)
-    }
-  }
-
-  const logoutUser = async () => {
-    const dispatchReturn = dispatch(logout())
-
-    await sendFetch({
-      token: user.token,
-      route: '/token',
-      method: 'DELETE'
-    })
-
-    return new Promise((resolve, reject) => {
-      dispatchReturn.then(
-        () => resolve('')
-      )
-    })
-  }
+  const loginUser = (...args) => login(dispatch, sendFetch, ...args)
+  const logoutUser = (...args) => logout(dispatch, sendFetch, user.token, ...args)
 
   const changeProgram = async () => {
     const dispatchReturn = dispatch(changeSelectProgram())
---
+++ src/hooks/useUser/login.js
@@ -0,0 +1,55 @@
+import {
+  login,
+  logout
+} from 'features/userSlice'
+
+import { VALID_EMAIL } from 'utils'
+
+export const loginUser = async (dispatch, sendFetch, { email, password }, setStatus) => {
+  if (!email && !password) return setStatus('noData')
+  if (!email || !VALID_EMAIL.test(email)) return setStatus('noEmail', 'Por favor, ingrese un correo valido')
+  if (!password) return setStatus('noPassword', 'Por favor, ingrese la contraseña')
+
+  const bodyJson = {
+    email,
+    password,
+    device_name: 'app_mobile'
+  }
+
+  try {
+    setStatus('loading')
+
+    const data = await sendFetch({
+      route: '/token',
+      method: 'POST',
+      bodyJson
+    })
+
+    if (data && data.access_token && !data.errors) {
+      setStatus('success')
+      return dispatch(login(data))
+    } else if (data && !data.errors) return setStatus('error')
+
+    if (data.errors.email) return setStatus('errorEmail', 'Usuario no encontrado')
+    setStatus('errorPassword', 'Contraseña Incorrecta')
+  } catch (error) {
+    setStatus('error')
+    console.log('Ocurrio un Error => ', error)
+  }
+}
+
+export const logoutUser = async (dispatch, sendFetch, token) => {
+  const dispatchReturn = dispatch(logout())
+
+  await sendFetch({
+    token,
+    route: '/token',
+    method: 'DELETE'
+  })
+
+  return new Promise((resolve, reject) => {
+    dispatchReturn.then(
+      () => resolve('')
+    )
+  })
+}
