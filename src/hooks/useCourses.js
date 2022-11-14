9ffa2be2d942afb8b33b6fae81a1dd081f8f3493
LuisFOsG <luisfosgdev@gmail.com>
2022-11-14 15:03:28 -0500

    feat: obteniendo preguntas y verificación inicial del estado

--- src/hooks/useCourses.js
+++ src/hooks/useCourses.js
@@ -6,41 +6,79 @@ import { setCourses } from 'features/coursesSlice'
 import useStatus from 'hooks/useStatus'
 import useFetch from 'hooks/useFetch'
 
+const getCourses = async ({ dispatch, token, sendFetch, setStatus }) => {
+  setStatus('loading')
+
+  const data = await sendFetch({
+    route: '/group',
+    method: 'GET',
+    token
+  })
+
+  const questions = await sendFetch({
+    route: '/survey',
+    method: 'GET',
+    token
+  })
+
+  dispatch(
+    setCourses(
+      await Promise.all(
+        data.map(async course => {
+          const dataAnswers = await sendFetch({
+            route: `/answer?group_id=${course.id}`,
+            method: 'GET',
+            token
+          })
+
+          const answers = dataAnswers.map((ans, i) => ({
+            num: i + 1,
+            value: ans.value
+          }))
+
+          let state = ''
+          if (answers.length === 0 && questions.length > 0) state = 'start'
+          if (questions.length === answers.length) state = 'end'
+          if (state === '') state = 'continue'
+
+          return {
+            image: 'https://placehold.jp/3e3e41/ffffff/150x150.png',
+            id: course.id,
+            name: course.subject.name,
+            teacher: course.professor.name,
+            program: 'info',
+            answers,
+            state
+          }
+        })
+      )
+    )
+  )
+
+  setStatus('')
+}
+
 const useCourses = (stateCorses) => {
   const { sendFetch } = useFetch()
   const dispatch = useDispatch()
   const { status, setStatus } = useStatus()
 
-  const user = useSelector(state => state.user)
-  const courses = useSelector(state => state.courses.coursesList.map(course => {
-    return {
-      image: 'https://placehold.jp/3e3e41/ffffff/150x150.png',
-      id: course.id,
-      name: course.subject.name,
-      teacher: course.professor.name,
-      program: 'info',
-      state: 'start'
-    }
-  }).filter(course => course.state === stateCorses))
+  const token = useSelector(state => state.user.token)
+  const courses = useSelector(state => state.courses.coursesList)
 
   useEffect(() => {
-    if (courses.length === 0) getCourses()
+    if (courses.length === 0) {
+      getCourses({
+        dispatch,
+        token,
+        sendFetch,
+        setStatus
+      })
+    }
   }, [])
 
-  const getCourses = async () => {
-    setStatus('loading')
-    const data = await sendFetch({
-      route: '/group',
-      method: 'GET',
-      token: user.token
-    })
-
-    dispatch(setCourses(data))
-    setStatus('')
-  }
-
   return {
-    courses,
+    courses: courses.filter(course => course.state === stateCorses),
     isLoading: status === 'loading'
   }
 }
